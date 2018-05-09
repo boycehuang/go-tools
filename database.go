20c5fe1a86d62e7b754d837576a7fd257b270dfe
Boyce <boyce@morezig.com>
2018-05-09 17:39:41 +0800

    update public funcion command

--- database.go
+++ database.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"   //
 )
 
+// OpenDB get db connections
 func OpenDB(config database.ConnectionConfig) (db *gorm.DB, err error) {
 	db, err = gorm.Open(config.Dialect, config.URI())
 	return
