a7146a4987e30b2dbc4a63ce0222021c2cfbbd6e
boyce <boyce.huang@cenoq.com>
2018-05-10 12:04:57 +0800

    add cockroachdb support

--- database/conn_conf.go
+++ database/conn_conf.go
@@ -6,21 +6,37 @@ type ConnectionConfig struct {
 	Dialect  string
 	Filepath string //sqlite only
 	Host     string
+	Port     string
 	User     string
 	Pass     string
 	Dbname   string
+	Ssl      bool
 }
 
 func (cc ConnectionConfig) URI() (uri string) {
-
-	if cc.Dialect == MYSQL {
-		uri = fmt.Sprintf("%s:%s@%s/%s?charset=utf8&parseTime=True&loc=Local", cc.User, cc.Pass, cc.Host, cc.Dbname)
-	} else if cc.Dialect == POSTGRES {
-		uri = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", cc.Host, cc.User, cc.Dbname, cc.Pass)
-	} else if cc.Dialect == SQLITE {
+	switch dialect := cc.Dialect; dialect {
+	case MYSQL:
+		if cc.Port != "" {
+			cc.Port = "3306"
+		}
+		uri = fmt.Sprintf("%s:%s@%s:%s/%s?charset=utf8&parseTime=True&loc=Local", cc.User, cc.Pass, cc.Host, cc.Port, cc.Dbname)
+	case POSTGRES:
+		if cc.Port != "" {
+			cc.Port = "5432"
+		}
+		uri = fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", cc.Host, cc.Port, cc.User, cc.Dbname, cc.Pass)
+	case MSSQL:
+		if cc.Port != "" {
+			cc.Port = "1443"
+		}
+		uri = fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s", cc.User, cc.Pass, cc.Host, cc.Port, cc.Dbname)
+	case COCKROACHDB:
+		if cc.Port != "" {
+			cc.Port = "26257"
+		}
+		uri = fmt.Sprintf("postgresql://%s@%s:%s/%s?sslmode=disable", cc.User, cc.Host, cc.Port, cc.Dbname)
+	case SQLITE:
 		uri = cc.Filepath
-	} else if cc.Dialect == MSSQL {
-		uri = fmt.Sprintf("sqlserver://%s:%s@%s?database=%s", cc.User, cc.Pass, cc.Host, cc.Dbname)
 	}
 
 	return
